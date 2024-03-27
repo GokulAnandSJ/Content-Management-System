46e7c5bc51482c901ceb9c65b7059211f005051e
GokulAnandSJ <gokulanand0110@gmail.com>
2024-03-27 11:27:41 +0530

    devI

--- src/main/java/com/example/cms/controller/UserController.java
+++ src/main/java/com/example/cms/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.cms.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cms.DAO.UserRequestDTO;
@@ -42,5 +44,6 @@ public class UserController {
 	public String test() {
 		return "Hellow From CMS";
 	}
-
+//	@DeleteMapping("/users/{userId}")
+//	public ResponseEntity<ResponseStructure<UserResponse>> softDelete(@RequestParam userId)
 }
